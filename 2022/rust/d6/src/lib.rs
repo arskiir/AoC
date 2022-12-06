039c74aed8a29138f6ef514352fe19d62210cca9
Arm Sukrit <arm.xdsk@gmail.com>
2022-12-06 22:43:00 +0700

    improve 2022 rust d6

--- 2022/rust/d6/src/lib.rs
+++ 2022/rust/d6/src/lib.rs
@@ -1,6 +1,18 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{BTreeSet, HashSet, VecDeque};
 
-pub fn solution(input: &str, unique_amount: u32) -> u32 {
+pub fn better_solution(input: &str, window_size: usize) -> usize {
+    input
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(window_size)
+        .enumerate()
+        .find(|(_, window)| window.iter().collect::<BTreeSet<_>>().len() == window.iter().len())
+        .unwrap()
+        .0
+        + window_size
+}
+
+pub fn poor_solution(input: &str, unique_amount: u32) -> u32 {
     let mut buffer = input.chars();
 
     let mut unique: HashSet<char> = HashSet::new();
@@ -38,29 +50,57 @@ mod tests {
 
     #[test]
     fn example_part1_works() {
-        assert_eq!(solution("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
-        assert_eq!(solution("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
-        assert_eq!(solution("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10);
-        assert_eq!(solution("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), 11);
+        assert_eq!(poor_solution("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
+        assert_eq!(poor_solution("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
+        assert_eq!(poor_solution("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10);
+        assert_eq!(poor_solution("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), 11);
     }
 
     #[test]
     fn part1_works() {
         let input = fs::read_to_string("./input.txt").unwrap();
-        assert_eq!(solution(&input, 4), 1855);
+        assert_eq!(poor_solution(&input, 4), 1855);
     }
 
     #[test]
     fn example_part2_works() {
-        assert_eq!(solution("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
-        assert_eq!(solution("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
-        assert_eq!(solution("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29);
-        assert_eq!(solution("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), 26);
+        assert_eq!(poor_solution("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
+        assert_eq!(poor_solution("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
+        assert_eq!(poor_solution("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29);
+        assert_eq!(poor_solution("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), 26);
     }
 
     #[test]
     fn part2_works() {
         let input = fs::read_to_string("./input.txt").unwrap();
-        assert_eq!(solution(&input, 14), 1855);
+        assert_eq!(poor_solution(&input, 14), 3256);
+    }
+
+    #[test]
+    fn better_example_part1_works() {
+        assert_eq!(better_solution("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
+        assert_eq!(better_solution("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
+        assert_eq!(better_solution("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10);
+        assert_eq!(better_solution("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), 11);
+    }
+
+    #[test]
+    fn better_part1_works() {
+        let input = fs::read_to_string("./input.txt").unwrap();
+        assert_eq!(poor_solution(&input, 4), 1855);
+    }
+
+    #[test]
+    fn better_example_part2_works() {
+        assert_eq!(better_solution("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
+        assert_eq!(better_solution("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
+        assert_eq!(better_solution("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29);
+        assert_eq!(better_solution("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), 26);
+    }
+
+    #[test]
+    fn better_part2_works() {
+        let input = fs::read_to_string("./input.txt").unwrap();
+        assert_eq!(better_solution(&input, 14), 3256);
     }
 }
