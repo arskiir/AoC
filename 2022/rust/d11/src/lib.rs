5e1f618bd569645fafcbb55bb9262d6038526c4b
Sukrit Banthao <arm.xdsk@gmail.com>
2022-12-11 23:25:21 +0700

    fff

--- 2022/rust/d11/src/lib.rs
+++ 2022/rust/d11/src/lib.rs
@@ -1,70 +1,14 @@
-#[derive(Debug)]
-enum OpOperand {
-    Old,
-    Num(i32),
-}
-
-#[derive(Debug)]
-enum Op {
-    Add(OpOperand),
-    Mul(OpOperand),
-}
-
-#[derive(Debug)]
-pub struct Monkey {
-    items: Vec<i32>,
-    op: Op,
-    divisible_by: i32,
-    true_to: u32,
-    false_to: u32,
-}
-
-impl Monkey {
-    pub fn take_turn(&mut self) {
-        for item in self.items.iter_mut() {
-            // do operation
-            match &self.op {
-                Op::Add(operand) => match operand {
-                    OpOperand::Old => {
-                        *item += *item;
-                    }
-                    OpOperand::Num(num) => {
-                        *item += num;
-                    }
-                },
-                Op::Mul(operand) => match operand {
-                    OpOperand::Old => {
-                        *item *= *item;
-                    }
-                    OpOperand::Num(num) => {
-                        *item *= num;
-                    }
-                },
-            }
-            // gets bored
-            *item /= 3;
-            // throw
-            if *item % self.divisible_by == 0 {
-            } else {
-            }
-        }
-    }
-
-    pub fn receive(&mut self, items: Vec<i32>) {}
-}
-
 fn parse_input(input: &str) -> Vec<Monkey> {
     input
         .split("\r\n\r\n")
         .map(|split| {
             let mut lines = split.lines();
-            lines.next(); // we don't need the monkey number line
+            lines.next(); // we don't need the monkey number
             lines.map(|l| l.trim_start())
         })
         .enumerate()
-        .map(|(monkey_num, mut monkey_lines)| {
+        .map(|(id, mut monkey_lines)| {
             let items_line = monkey_lines.next().unwrap();
-            dbg!(&items_line);
             let items = items_line
                 .strip_prefix("Starting items: ")
                 .unwrap()
@@ -120,24 +64,115 @@ fn parse_input(input: &str) -> Vec<Monkey> {
                 true_to,
                 false_to,
                 op,
+                id,
+                inspect_count: 0,
             }
         })
         .collect()
-    // vec![]
+}
+
+#[derive(Debug)]
+enum OpOperand {
+    Old,
+    Num(i32),
+}
+
+#[derive(Debug)]
+enum Op {
+    Add(OpOperand),
+    Mul(OpOperand),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct ItemMail {
+    pub recipient: usize,
+    pub item: i32,
+}
+
+#[derive(Debug)]
+pub struct Monkey {
+    id: usize,
+    items: Vec<i32>,
+    op: Op,
+    divisible_by: i32,
+    true_to: usize,
+    false_to: usize,
+    inspect_count: u32,
+}
+
+impl Monkey {
+    pub fn take_turn(&mut self) -> Vec<ItemMail> {
+        let mut thrown_items = Vec::new();
+        for item in self.items.iter_mut() {
+            // do operation
+            match &self.op {
+                Op::Add(operand) => match operand {
+                    OpOperand::Old => {
+                        *item += *item;
+                    }
+                    OpOperand::Num(num) => {
+                        *item += num;
+                    }
+                },
+                Op::Mul(operand) => match operand {
+                    OpOperand::Old => {
+                        *item *= *item;
+                    }
+                    OpOperand::Num(num) => {
+                        *item *= num;
+                    }
+                },
+            }
+            // gets bored
+            *item /= 3;
+            // throw
+            thrown_items.push(ItemMail {
+                item: *item,
+                recipient: if *item % self.divisible_by == 0 {
+                    self.true_to
+                } else {
+                    self.false_to
+                },
+            });
+            // increment the inspect count
+            self.inspect_count += 1;
+        }
+
+        dbg!(&thrown_items);
+        thrown_items
+    }
+
+    pub fn receive(&mut self, mut items: Vec<ItemMail>) -> Vec<ItemMail> {
+        if items.len() != 0 {
+            for item in items.iter() {
+                if item.recipient == self.id {
+                    self.items.push(item.item);
+                }
+            }
+            items = items
+                .iter()
+                .filter(|item| item.recipient != self.id)
+                .map(|item| *item)
+                .collect();
+        }
+        items
+    }
 }
 
 pub fn part1(input: &str) -> u32 {
     let mut monkeys = parse_input(&input);
-    // for m in monkeys {
-    //     dbg!(m);
-    // }
     let rounds = 20;
+    let mut prev_thrown_items: Vec<ItemMail> = Vec::new();
     for _ in 0..rounds {
+        // dbg!(&monkeys);
         for monkey in monkeys.iter_mut() {
-            monkey.take_turn();
-            monkeys[0].receive(vec![]);
+            prev_thrown_items = monkey.receive(prev_thrown_items);
+            let mut thrown_items = monkey.take_turn();
+            prev_thrown_items.append(&mut thrown_items);
         }
     }
+
+    // dbg!(monkeys);
     !todo!()
 }
 
